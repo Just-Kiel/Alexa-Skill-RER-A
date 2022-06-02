bf9b9e6fac57b1ba4774533cf6ff596dd81adafa
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-06-02 19:14:25 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -8,7 +8,7 @@ module.exports.fetchHourApi = async function fetchHourApi(destination) {
     if(destination === "Marne-la-Vallee Chessy"){
         dest = "R";
     } else {
-        dest = "A+R";
+        dest = "A%2BR";
     }
     
     let url = endpoint + '/v4/schedules/rers/A/Noisiel/' + dest;
