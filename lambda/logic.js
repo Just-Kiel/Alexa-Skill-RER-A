36d1cd984d36e131eeecbf7052b61522f7bcb963
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-06-02 19:47:51 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -1,15 +1,15 @@
 const axios = require('axios');
 
-module.exports.fetchHourApi = async function fetchHourApi() {
+module.exports.fetchHourApi = async function fetchHourApi(destination) {
     let endpoint = 'https://api-ratp.pierre-grimaud.fr';
     
-    let dest = "A%2BR";
+    let dest = "";
     
-    // if(destination === "Marne-la-Vallee Chessy"){
-        // dest = "R";
-    // } else {
-        // dest = "A%2BR";
-    // }
+    if(destination === "Marne-la-Vallee Chessy"){
+        dest = "R";
+    } else {
+        dest = "A%2BR";
+    }
     
     let url = endpoint + '/v4/schedules/rers/A/Noisiel/' + dest;
 
