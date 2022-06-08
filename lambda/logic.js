170b68f8c5e0c7be478d9d08724673730120ebaa
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2022-06-08 15:19:14 +0200

    test split might break everything

--- lambda/logic.js
+++ lambda/logic.js
@@ -40,11 +40,11 @@ module.exports.fetchHourApiForSpecificDeparture = async function fetchHourApiFor
 
     start = start.split(" ");
 
-    let depart = start[0];
+    let depart = start[1];
 
-    for(let i = 1; i<start.length; i++){
-        depart.concat('+', start[i]);
-    }
+    // for(let i = 1; i<start.length; i++){
+    //     depart.concat('+', start[i]);
+    // }
     
     let url = endpoint + '/v4/schedules/rers/A/' + start + '/' + dest;
 
